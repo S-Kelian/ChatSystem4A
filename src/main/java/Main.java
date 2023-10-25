efa0c9438efea382fe6bba3ce5bd0e00638d4b15
sebaici <kelian.sbc@gmail.com>
2023-10-25 11:54:36 +0200

    UDP Listener

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import UDP.UDPListener;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        UDPListener listener = new UDPListener();
+        listener.start();
     }
 
 }
---
+++ src/main/java/UDP/UDPListener.java
@@ -0,0 +1,43 @@
+package UDP;
+
+import java.io.IOException;
+import java.net.*;
+
+public class UDPListener {
+    Thread thread;
+
+    public static void log(Object o) {
+        Thread thread = Thread.currentThread();
+        System.out.println("[" + thread.getName() + "] " + o);
+    }
+
+    private static class GreeterServer extends Thread {
+        @Override
+        public void run() {
+            boolean running = true;
+            int port = 1789;
+            try (DatagramSocket socket = new DatagramSocket(port)) {
+                byte[] bufferRecv = new byte[1024];
+                DatagramPacket packet = new DatagramPacket(bufferRecv, bufferRecv.length);
+                while (running) {
+                    socket.receive(packet);
+                    String message = new String(packet.getData(), 0, packet.getLength());
+                    log("Greetings "+message+" !!!");
+                    if (message.equals("bye")) {
+                        running = false;
+                    }
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+    public UDPListener() {
+        thread = new GreeterServer();
+    }
+
+    public void start() {
+        thread.start();
+    }
+}
