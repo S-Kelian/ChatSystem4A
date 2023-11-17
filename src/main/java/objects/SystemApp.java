4ef63f320460b7b50374b5d4e6f0dbbec81043dd
TzT_laptop <theozt25@gmail.com>
2023-11-17 16:11:33 +0100

    systemappcreated

---
+++ src/main/java/objects/SystemApp.java
@@ -0,0 +1,31 @@
+package main.java.objects;
+import java.util.ArrayList;
+
+import UDP.UDPListener;
+import UDP.UDPSender;
+import objects.User;
+
+public class SystemApp {
+    private User me;
+    private ArrayList<User> contactList;
+    private UDPListener listener;
+    private UDPSender udpUnicast;
+    private UDPSender udpBroadcast;
+
+    public SystemApp(User me) {
+        this.me = me;
+        this.contactList = new ArrayList<User>();
+        this.listener = new UDPListener();
+        this.udpUnicast = new UDPSender(me.getIp(), me.getPort());
+        this.udpBroadcast = new UDPSender(me.getIp(), me.getPort());
+    }
+
+    public void start() {
+        listener.start();
+        udpUnicast.start();
+        udpBroadcast.start();
+    }
+
+
+
+}
