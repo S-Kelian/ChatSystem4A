c710ddccc3188cc283bf0329dc03c7cff0de821c
Kelian S <kelian.sbc@gmail.com>
2023-11-19 19:30:54 +0100

    Modif vue du Chat avec mise a jour des users

--- src/main/java/Vues/Chat.java
+++ src/main/java/Vues/Chat.java
@@ -3,62 +3,93 @@ package Vues;
 import objects.SystemApp;
 
 import javax.swing.*;
+import java.awt.*;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Chat {
 
-    SystemApp app = SystemApp.getInstance();
+    // Instance of the application system
+    private final SystemApp app = SystemApp.getInstance();
 
+    // List of labels representing online users
+    private final List<JLabel> usersOnline = new ArrayList<>();
+
+    // Constructor
     public Chat() throws SocketException, UnknownHostException {
     }
 
-    public void create(){
-        System.out.println("Ouverture de la fenetre de chat");
+    // Method to create and display the chat window
+    public void create() {
+
         JFrame frame = new JFrame("Chat");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 300);
+        //set max size
+        frame.setSize(500, 500);
 
-        JPanel panel = new JPanel();
+        JPanel mainPanel = new JPanel();
         JPanel panelNickname = new JPanel();
-        JLabel label = new JLabel("Welcome " + app.getMe().getNickname());
-
-        panelNickname.add(label);
-        panel.add(panelNickname);
-        panel.setAlignmentX(JPanel.CENTER_ALIGNMENT);
-        frame.add(panel);
-
-
+        JLabel titleLabel = new JLabel("Welcome " + app.getMe().getNickname());
         JPanel panelUsersOnline = new JPanel();
-        // get users online list
         JLabel labelUsersOnline = new JLabel("Users online : " + app.getUsersOnline().size());
-        panelUsersOnline.add(labelUsersOnline);
-        panelUsersOnline.setAlignmentY(JPanel.CENTER_ALIGNMENT);
-        JLabel[] usersOnline = new JLabel[app.getUsersOnline().size()];
-        for (int i = 0; i < app.getUsersOnline().size(); i++) {
-            usersOnline[i] = new JLabel(app.getUsersOnline().get(i).getNickname());
-            if (app.getUsersOnline().get(i).getNickname().equals(app.getMe().getNickname())) {
-                usersOnline[i].setText(usersOnline[i].getText() + " (you)");
-            }
-            panelUsersOnline.add(usersOnline[i]);
-        }
         JButton refresh = new JButton("Refresh");
+
+        // Set properties
+        mainPanel.setLayout(new BorderLayout());
+        panelUsersOnline.setLayout(new BorderLayout());
+
+        //add components
+        panelUsersOnline.add(labelUsersOnline , BorderLayout.PAGE_START);
+        panelUsersOnline.add(refresh, BorderLayout.PAGE_END);
+        panelNickname.add(titleLabel);
+
+        mainPanel.add(panelNickname, BorderLayout.PAGE_START);
+        mainPanel.add(panelUsersOnline, BorderLayout.LINE_END);
+        frame.add(mainPanel);
+
+        // Create the list of online users
+        createLabelsOfUsers(panelUsersOnline);
+
+        // Events
         refresh.addActionListener(e -> {
-            // get users online list
+            // Refresh the list of online users
+            usersOnline.clear();
+            panelUsersOnline.removeAll();
             labelUsersOnline.setText("Users online : " + app.getUsersOnline().size());
-            for (int i = 0; i < app.getUsersOnline().size(); i++) {
-                usersOnline[i].setText(app.getUsersOnline().get(i).getNickname());
-                if (app.getUsersOnline().get(i).getNickname().equals(app.getMe().getNickname())) {
-                    usersOnline[i].setText(usersOnline[i].getText() + " (you)");
-                }
-            }
+            panelUsersOnline.add(labelUsersOnline , BorderLayout.PAGE_START);
+            panelUsersOnline.add(refresh, BorderLayout.PAGE_END);
+            createLabelsOfUsers(panelUsersOnline);
+            panelUsersOnline.revalidate();
+            panelUsersOnline.repaint();
         });
-        panelUsersOnline.add(refresh);
-        panelUsersOnline.setAlignmentX(JPanel.CENTER_ALIGNMENT);
-        panel.add(panelUsersOnline);
 
-
-
+        // Making the window visible
         frame.setVisible(true);
+    }
+
+    /**
+     * Method to create user labels and add them to the list and the panel
+     */
+    private void createLabelsOfUsers(JPanel panelUsersOnline) {
+
+        JPanel usersOnlineList= new JPanel();
+        usersOnlineList.setLayout(new BoxLayout(usersOnlineList, BoxLayout.Y_AXIS));
+        for (int i = 0; i < app.getUsersOnline().size(); i++) {
+            JLabel userLabel = new JLabel(app.getUsersOnline().get(i).getNickname());
+
+            // Add "(you)" to the label of the current user
+            if (app.getUsersOnline().get(i).getNickname().equals(app.getMe().getNickname())) {
+                userLabel.setText(userLabel.getText() + " (you)");
+            }
+
+            // Add the label to the list and the panel
+            usersOnline.add(userLabel);
+        }
+        for (JLabel userLabel : usersOnline) {
+            usersOnlineList.add(userLabel);
+        }
+        panelUsersOnline.add(usersOnlineList, BorderLayout.CENTER);
     }
 }
